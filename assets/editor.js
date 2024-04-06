a6917f4c1a1314c6a80d03903330e9fe82a1315d
DeviousPear <deviouspear@pearnet.xyz>
2024-04-06 10:04:39 -0700

    added material editor

--- assets/editor.js
+++ assets/editor.js
@@ -1,27 +1,28 @@
-// import { WebContainer } from 'https://esm.sh/@webcontainer/api';
-// const webcontainerInstance = await WebContainer.boot();
+import { WebContainer } from 'https://esm.sh/@webcontainer/api';
+const webcontainerInstance = await WebContainer.boot();
+import * as BABYLON from 'https://esm.sh/@babylonjs/core/Legacy/legacy';
 //Set up Monaco
 require.config({ paths: { 'vs': 'https://cdnjs.cloudflare.com/ajax/libs/monaco-editor/0.26.1/min/vs' }});
 require(["vs/editor/editor.main"], () => {
-  let editor = monaco.editor.create(document.getElementById('monaco'), {
+  let editor = monaco.editor.create(document.getElementById('monaco-editor'), {
     value: `console.log("Hello World!")`,
     language: 'javascript',
     theme: 'vs-dark',
   })
-  window.onresize = editor.layout
-  setTimeout(() => {document.getElementById("splash").classList.add("hidden")}, 4500)
+  window.addEventListener("resize", () => editor.layout())
+//   setTimeout(() => {document.getElementById("splash").remove()}, 4500)
 });
 //When clicked on the create buttons
 function createMaterial() {
     let name = prompt("Name your material", "myMaterial")
     if (name) {
-            // webcontainerInstance.fs.writeFile("/src/materials/" + name, `
-            // import * as BABYLON from "https://esm.sh/babylonjs"
-            // const material = 
+            webcontainerInstance.fs.writeFile("/src/materials/" + name, `
+            import * as BABYLON from "https://esm.sh/babylonjs"
+            const material = 
             
-            // new BABYLON.StandardMaterial()
-            // export default material;
-            // `)
+            new BABYLON.StandardMaterial()
+            export default material;
+            `)
     }}
 //its esm, so i need to expose it as a global
 Object.defineProperty(window, "createMaterial", {
@@ -62,63 +63,37 @@ const resizeObserver = new ResizeObserver(() => {
     let assetsRect = document.getElementById("assets").getBoundingClientRect()
     let scenesRect = document.getElementById("scenes").getBoundingClientRect()
     document.getElementById("materials").style.height = `calc(100% - ${scenesRect.height + assetsRect.height}px)`
-  });
+  })
   
-  resizeObserver.observe(document.getElementById("scenes"));
-  resizeObserver.observe(document.getElementById("assets"));
-  
+resizeObserver.observe(document.getElementById("scenes"))
+resizeObserver.observe(document.getElementById("assets"))
 
+//Material Editor
+const canvas = document.getElementById("material-viewer")
+const engine = new BABYLON.Engine(canvas, true)
+const scene = new BABYLON.Scene(engine)
+const mat = new BABYLON.StandardMaterial()
+const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
+const box = BABYLON.MeshBuilder.CreateBox("box", {size: 3}, scene)
+box.material = mat
+const camera = new BABYLON.ArcRotateCamera("Camera", 0, 0, 10, new BABYLON.Vector3(0, 0, 0), scene)
+camera.setPosition(new BABYLON.Vector3(0, 0, 20))
+camera.attachControl(canvas, false, false)
+// const advancedTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("myUI");
 
+// var button1 = BABYLON.GUI.Button.CreateSimpleButton("but1", "Click Me");
+// button1.width = "150px"
+// button1.height = "40px";
+// button1.color = "white";
+// button1.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_TOP;
+// advancedTexture.addControl(button1);    
+engine.runRenderLoop(() => scene.render())
+window.addEventListener("resize", function () {
+    engine.resize();
+});
 
-
-
-
-
-
-
-//CheerpX version, not being used
-//let esbuildPath;
-// const cx = await CheerpXApp.create({
-//     devices: [
-//       {
-//         name: "block1",
-//         type: "block",
-//         url: "https://disks.webvm.io/debian_large_20230522_5044875331.ext2",
-//       },
-//     ],
-//     mounts: [
-//       { type: "ext2", dev: "block1", path: "/" },
-//       { type: "cheerpOS", dev: "/app", path: "/app" },
-//       { type: "cheerpOS", dev: "/str", path: "/data" },
-//       { type: "devs", dev: "", path: "/dev" },
-//     ],
-//   });
-// cx.setConsole(document.getElementById("console"));
-// cx.run("curl", ["-O", "https://registry.npmjs.org/esbuild-linux-64/-/esbuild-linux-64-0.0.9.tgz"], {
-//     env: [
-//       "HOME=/home/user",
-//       "USER=user",
-//       "SHELL=/bin/bash",
-//       "EDITOR=vim",
-//       "LANG=en_US.UTF-8",
-//       "LC_ALL=C",
-//     ],
-//     cwd: "/home/user",
-//     uid: 1000,
-//     gid: 1000,
-// }).then(() => {
-//     cx.run("tar", ["xf", "./esbuild-linux-64-0.0.9.tgz"], {
-//         env: [
-//             "HOME=/home/user",
-//             "USER=user",
-//             "SHELL=/bin/bash",
-//             "EDITOR=vim",
-//             "LANG=en_US.UTF-8",
-//             "LC_ALL=C",
-//           ],
-//           cwd: "/home/user"
-//     }).then(() => {
-//         esbuildPath = "./package/bin/esbuild"
-        
-//     })
-// })
+document.getElementById("material-properties").oninput = () => {
+    if (document.getElementById("dif-ttr").value) mat.diffuseTexture = new BABYLON.Texture("https://corsproxy.io/?" + encodeURIComponent(document.getElementById("dif-ttr").value))
+    let color = document.getElementById("dif-clr").value
+    mat.diffuseColor = BABYLON.Color3.FromHexString(color)
+}
